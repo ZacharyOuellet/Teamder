ec7c991d9714855640a15da8a459bf9014e296b6
ZacharyOuellet <113061957+ZacharyOuellet@users.noreply.github.com>
2023-02-04 17:39:54 -0500

    fichiers initiaux

---
+++ main.py
@@ -0,0 +1,5 @@
+from profil import*
+
+
+if __name__ == '__main__':
+    print("hello world")
---
+++ profil.py
@@ -0,0 +1,6 @@
+class Profil:
+    def __init__(self, name:str, age:int, speaksFrench:bool, speaksEnglish:bool, ):
+        self.name = name
+        self.age = age
+        self.speaksFrench = speaksFrench
+        self.speaksEnglish = speaksEnglish
