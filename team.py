5e01d8a411c2f93a2be5e585c130f0adb7e9d26e
Charlesbrsd <charles.brsd@gmail.com>
2023-02-04 23:52:38 -0500

    push1charles

--- team.py
+++ team.py
@@ -16,3 +16,21 @@ class Team:
         result = f"{self.__name} :\n"
         for member in self.members:
             result += f"{member.name}     "
+    
+    def computeScore(self):    
+        score=0
+        for member1 in self.members:
+            for member2 in self.members:
+                if member1!=member2:
+                    if not ((member1.speaksFrench == member2.speaksFrench==True) or (member1.speaksEnglish==member2.speaksEnglish==True)):
+                        score+=10;
+                    else:
+                        score-=10;
+                    if member1.goal==member2.goal:
+                        score+=5;
+                    else:
+                        score-=5;
+
+                    for i in range( 0, len()):
+                        if i in #liste des langages
+                            score +=2;
